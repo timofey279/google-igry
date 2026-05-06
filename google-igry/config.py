c791e753236702c8c73bf6443bef9715d2c68edb
timofey279 <timkobuilder@gmail.com>
2026-05-06 20:34:31 +0300

    Delete google-igry directory

--- google-igry/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = "secret-key"
-    SQLALCHEMY_DATABASE_URI = "sqlite:///db.sqlite3"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-    UPLOAD_FOLDER = os.path.join("app", "static", "uploads")
